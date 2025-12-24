9a060b68753efc8f0b839459bc07a9eb0ee379b8
Samia-Mohsin <samiaazam50@gmail.com>
2025-12-24 23:15:00 +0500

    Update docusaurus.config.ts

--- frontend-book/docusaurus.config.ts
+++ frontend-book/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://samia.github.io',
+  url: 'https://humanoid-robotics-textbook-hackatho-seven.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
